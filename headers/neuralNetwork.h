7bf1417e47a70b29f968660ebc711035cf163b79
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-15 08:02:22 +0530

    neural n/w struct + constructor + helpers

--- headers/neuralNetwork.h
+++ headers/neuralNetwork.h
@@ -1,6 +1,7 @@
 #ifndef neuralNetwork_H
 #define neuralNetwork_H
 
+// Layer struct
 typedef struct Layer{
     int input_size;
     int output_size;
@@ -9,5 +10,18 @@ typedef struct Layer{
     double *outputs;
     double *deltas;
 } Layer; 
+
+// Neural network struct
+typedef struct Network {
+    Layer *layers;
+    int num_layers; 
+} Network;
+
+// Network constructors
+Network createNetwork(int input_size, int *hidden_sizes, int num_hidden, int output_sizes);
+void freeNetwork(Network *net); // Network Mem Free
+void forward(Network* net, double *input); // FW chaining
+void backward(Network *net, double *input, double *target, double learning_rate); // Backward chaining
+double meanSquaredError(double *output, double *target, int size);
 
 #endif
