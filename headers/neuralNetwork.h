ffbc9978a4e4451df66b3cd943f3bc820ad480e0
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-15 12:40:40 +0530

    minor func definition changes

--- headers/neuralNetwork.h
+++ headers/neuralNetwork.h
@@ -17,11 +17,14 @@ typedef struct Network {
     int num_layers; 
 } Network;
 
-// Network constructors
+// Network constructor
 Network createNetwork(int input_size, int *hidden_sizes, int num_hidden, int output_sizes);
-void freeNetwork(Network *net); // Network Mem Free
-void forward(Network* net, double *input); // FW chaining
-void backward(Network *net, double *input, double *target, double learning_rate); // Backward chaining
+// Network Mem Free
+void freeNetwork(Network *net); 
+// FW chaining
+void forward(Network* net, double *input); 
+// Backward chaining
+void backward(Network *net, double *input, double *target, double learning_rate); 
 double meanSquaredError(double *output, double *target, int size);
 Layer createLayer(int input_size, int output_size);
 
